19e0396e209b68d44e3105054beafed0e1dc32db
Or Zellinger <or.zellinger@gmail.com>
2025-06-15 15:28:59 +0300

    Create Terrorists Folder And Types Of Terrorists

---
+++ InvastigationGame/Models/Terrorists/PrivateTerrorist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvastigationGame.Models.Terrorists
+{
+    public class PrivateTerrorist : Terrorist
+    {
+        public PrivateTerrorist() : base("private")
+        {
+
+        }
+    }
+}
