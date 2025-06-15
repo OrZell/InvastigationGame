f7d859c5710ea3bd46c116ab8f4ffe1c239f86f5
Or Zellinger <or.zellinger@gmail.com>
2025-06-15 15:16:21 +0300

    Create The Sensors Folder And The Basic Sensors

---
+++ InvastigationGame/Models/Sensors/MovementSensor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvastigationGame.Models.Sensors
+{
+    public class MovementSensor : Sensor
+    {
+        public MovementSensor() : base("movement")
+        {
+
+        }
+    }
+}
