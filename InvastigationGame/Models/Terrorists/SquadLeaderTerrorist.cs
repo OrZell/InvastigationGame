0067fb9efb34175bc84a904be180ad670225e226
Or Zellinger <or.zellinger@gmail.com>
2025-06-16 10:13:18 +0300

    Create Squad Leader Terrorist

---
+++ InvastigationGame/Models/Terrorists/SquadLeaderTerrorist.cs
@@ -0,0 +1,45 @@
+﻿using InvastigationGame.Models.Sensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvastigationGame.Models.Terrorists
+{
+    public class SquadLeaderTerrorist : Terrorist
+    {
+        public int AttackCounter = 0;
+        public SquadLeaderTerrorist() : base("squad leader")
+        {
+
+        }
+
+        public void Attack()
+        {
+            if (AttackCounter == 3)
+            {
+                RemoveActive();
+                AttackCounter = 0;
+            }
+        }
+
+        public void RemoveActive()
+        {
+            List<Sensor> ActiveSensors = new List<Sensor>();
+            Random Rand = new Random();
+
+            foreach (Sensor sensor in WeaknesSensors)
+            {
+                if (sensor.Active)
+                {
+                    ActiveSensors.Add(sensor);
+                }
+            }
+
+            Sensor sen = ActiveSensors.ElementAt(Rand.Next(ActiveSensors.Count));
+            sen.Active = false;
+        }
+    }
+}
--- InvastigationGame/ProgramStaticData/StaticData.cs
+++ InvastigationGame/ProgramStaticData/StaticData.cs
@@ -8,7 +8,7 @@ namespace InvastigationGame.ProgramStaticData
 {
     public class StaticData
     {
-        public static Dictionary<string, int> TypesAndMuchSensors = new Dictionary<string, int> { { "private", 2 } };
+        public static Dictionary<string, int> TypesAndMuchSensors = new Dictionary<string, int> { { "private", 2 }, {"squad leader", 4 };
 
         public static List<string> TypesOfSensors = new List<string> { { "movement" }, { "lighting" }, { "selolar" } };
     }
